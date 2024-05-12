3ddc9a3cb5324d41b3dd715cd865447ab28b0ad8
AtomJ2 <42699170+AtomJ2@users.noreply.github.com>
2024-05-12 18:22:32 +0700

    Add files via upload

---
+++ lab6/gpu/laplace2d.h
@@ -0,0 +1,15 @@
+class Laplace {
+private:
+    double *A, *Anew;
+    int m, n;
+
+public:
+    Laplace(int m, int n);
+    ~Laplace();
+    void initialize();
+    void calcNext();
+    double calcError();
+    void swap();
+};
+
+void printMatrix(double *matrix, int rows, int cols);
